acafcf914f9b7021e8dbb0a6541d8738fb12c89a
SG5143 <sun5143a@gmail.com>
2024-11-30 21:44:54 +0900

    Add code for initializing the game board in main.js - 게임보드 초기화 관련 코드 작성

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,69 @@
-function createGameBoard(row, col) {
+const TILE_PATH = 0;
+const TILE_WALL = 1;
+const TILE_PLAYER = 2;
+const TILE_BOX = 3;
+const TILE_GOAL = 4;
+const TILE_GOAL_IN = 5;
+
+// 맵(레벨) 데이터 json 파일 로드하기
+async function loadLevel(level) {
+    const res_json = await fetch(`javascript/levels/level${level}.json`);
+    return await res_json.json();
+}
+
+// 타일 이미지 이름 가져오기
+function getTileName(tileType) {
+    switch (tileType) {
+        case TILE_PATH:
+            return 'path';
+        case TILE_WALL:
+            return 'wall';
+        case TILE_PLAYER:
+            return 'player';
+        case TILE_BOX:
+            return 'box';
+        case TILE_GOAL:
+            return 'goal';
+        case TILE_GOAL_IN:
+            return 'goal_in';
+        default:
+            return 'path'; 
+    }
+}
+
+// 게임 보드 그리기
+function createGameBoard(levelData) {
     const board = document.createElement("div");
     board.id = "board";
 
-    for (let i = 0; i < row; i++) {
-        const div = document.createElement("div");
-        div.style.display = "flex";
-        div.id = "row" + i;
-        for (let j = 0; j < col; j++)
-            div.innerHTML += "<div class='map'></div>";
-        board.appendChild(div);
+    const rows = levelData.map.length;
+    const cols = levelData.map[0].length;
+
+    for (let row = 0; row < rows; row++) {
+        const rowDiv = document.createElement("div");
+        rowDiv.style.display = "flex";
+        rowDiv.id = `row-${row}`;
+        for (let col = 0; col < cols; col++) {
+            const cell = document.createElement("div");
+            cell.id = `cell-${row}-${col}`;
+            cell.className = getTileName(levelData.map[row][col]);
+
+            rowDiv.appendChild(cell);
+        }
+        board.appendChild(rowDiv);
     }
 
-    const game = document.getElementById("game");
-    game.appendChild(board);
+    const gameContainer = document.getElementById("game");
+    gameContainer.innerHTML = "";
+    gameContainer.appendChild(board);
 }
 
+async function initializeGame() {
+    const levelData = await loadLevel(1);
+    createGameBoard(levelData);
+}
+
+
 window.onload = () => {
-    createGameBoard(10, 10);
+    initializeGame();
 }
