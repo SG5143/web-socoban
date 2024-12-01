ce60094e9d8b42c454c5978a284ec5211dd7aa7e
SG5143 <sun5143a@gmail.com>
2024-12-02 00:19:43 +0900

    Add key handling, button events, and map drawing logic during movement in main.js - 키 핸들, 움직임, 맵그리기 등 로직 추가

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,5 @@
+import { Game } from './game.js';
+
 const TILE_PATH = 0;
 const TILE_WALL = 1;
 const TILE_PLAYER = 2;
@@ -5,13 +7,10 @@ const TILE_BOX = 3;
 const TILE_GOAL = 4;
 const TILE_GOAL_IN = 5;
 
-// 맵(레벨) 데이터 json 파일 로드하기
-async function loadLevel(level) {
-    const res_json = await fetch(`javascript/levels/level${level}.json`);
-    return await res_json.json();
-}
+let game;
+let currentLevel = 1; // 단계 -> 초기값은 1
 
-// 타일 이미지 이름 가져오기
+// 해당 타일의 파일이름 가져오기
 function getTileName(tileType) {
     switch (tileType) {
         case TILE_PATH:
@@ -27,17 +26,40 @@ function getTileName(tileType) {
         case TILE_GOAL_IN:
             return 'goal_in';
         default:
-            return 'path'; 
+            return 'path';
     }
 }
 
+function getTileClass(gameInstance, row, col, tileType) {
+    if (gameInstance.player.x === col && gameInstance.player.y === row)
+        return getTileName(TILE_PLAYER);
+
+    if (gameInstance.boxes.some(box => box.x === col && box.y === row)) {
+        return getTileName(
+            gameInstance.goals.some(goal => goal.x === col && goal.y === row) ? TILE_GOAL_IN : TILE_BOX
+        );
+    }
+
+    if (gameInstance.goals.some(goal => goal.x === col && goal.y === row))
+        return getTileName(TILE_GOAL);
+
+    return getTileName(tileType);
+}
+
+function updateCellClass(row, col, gameInstance) {
+    const cell = document.getElementById(`cell-${row}-${col}`);
+    const tileType = gameInstance.map.map[row][col];
+    cell.className = getTileClass(gameInstance, row, col, tileType);
+}
+
 // 게임 보드 그리기
-function createGameBoard(levelData) {
+function createGameBoard(gameInstance) {
+    const mapData = gameInstance.map;
     const board = document.createElement("div");
     board.id = "board";
 
-    const rows = levelData.map.length;
-    const cols = levelData.map[0].length;
+    const rows = mapData.height
+    const cols = mapData.width;
 
     for (let row = 0; row < rows; row++) {
         const rowDiv = document.createElement("div");
@@ -46,7 +68,9 @@ function createGameBoard(levelData) {
         for (let col = 0; col < cols; col++) {
             const cell = document.createElement("div");
             cell.id = `cell-${row}-${col}`;
-            cell.className = getTileName(levelData.map[row][col]);
+
+            const tileType = mapData.map[row] && mapData.map[row][col];
+            cell.className = getTileClass(gameInstance, row, col, tileType);
 
             rowDiv.appendChild(cell);
         }
@@ -56,14 +80,80 @@ function createGameBoard(levelData) {
     const gameContainer = document.getElementById("game");
     gameContainer.innerHTML = "";
     gameContainer.appendChild(board);
+    createMenuGroup();
 }
 
+function createMenuGroup(){
+    const gameContainer = document.getElementById("game");
+    gameContainer.innerHTML +=`
+    <div class="menu-group">
+        <div class="level-control">
+            <button id="decrease-level">-</button>
+            <span id="current-level">${currentLevel}</span>
+            <button id="increase-level">+</button>
+        </div>
+        <button id="set-level">선택 단계로 이동</button>
+    </div>
+    `;
+
+    document.getElementById('decrease-level').addEventListener('click', () => handleLevel(-1));
+    document.getElementById('increase-level').addEventListener('click', () => handleLevel(1));
+    document.getElementById('set-level').addEventListener('click', () => initializeGame());
+}
+
+function handleLevel(increment) {
+    const change = currentLevel + increment;
+    if (change > 0 && change <= 5) { // 일단 임의로 5단계 나중에 추가하면 바꾸기 
+        currentLevel = change;
+        updateLevel();
+    }
+}
+
+function updateLevel() {
+    document.getElementById('current-level').innerText = currentLevel;
+}
+
+
 async function initializeGame() {
-    const levelData = await loadLevel(1);
-    createGameBoard(levelData);
+    game = new Game();
+    await game.start(currentLevel);
+    createGameBoard(game);
 }
 
 
 window.onload = () => {
     initializeGame();
+    handleKey();
+}
+
+
+// 키 꾹 누르는거 방지하면서 입력 받는 메서드
+function handleKey() {
+    let keyPressed = false;
+
+    window.addEventListener("keydown", (e) => {
+        if (!keyPressed) {
+            console.log(e.key);
+            keyPressed = true;
+
+            const previousPlayerPosition = { x: game.player.x, y: game.player.y }; 
+            game.movePlayer(e.key);
+
+            updateCellClass(previousPlayerPosition.y, previousPlayerPosition.x, game); 
+            updateCellClass(game.player.y, game.player.x, game);
+            
+            game.boxes.forEach(box => {
+                updateCellClass(box.y, box.x, game);
+            });
+
+            game.goals.forEach(goal => {
+                updateCellClass(goal.y, goal.x, game);
+            });
+        }
+    });
+
+    // 키를 때야 다음 키 입력을 받을 수 있도록 하기 
+    window.addEventListener("keyup", () => {
+        keyPressed = false;
+    });
 }
